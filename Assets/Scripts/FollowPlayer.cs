0bbde257d4b729fd837882159ca0c15de0a3f924
Horacio Valdes <horavaldesg@gmail.com>
2021-06-26 19:01:21 -0500

    Initial commit

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public static bool gravityChange;
+    public GameObject player;
+    public GameObject cam2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gravityChange)
+        {
+            cam2.transform.position = new Vector3(player.transform.position.x, player.transform.position.y - player.transform.position.y + 1, player.transform.position.z);
+            cam2.transform.rotation = Quaternion.Euler(-90, 0, 0);
+            
+        }
+        else
+        {
+            cam2.transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 12, player.transform.position.z);
+            cam2.transform.rotation = Quaternion.Euler(90, 0, 0);
+        }
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z);
+        
+    }
+}
---
+++ Assets/Scripts/SwitchCamera.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchCamera : MonoBehaviour
+{
+    public GameObject camera1;
+    public GameObject camera2;
+    public static bool space;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && space)
+        {
+            Switch();
+        }
+    }
+
+    public void Switch()
+    {
+        //Top View
+        if (camera1.activeSelf == true)
+        {
+            camera2.SetActive(true);
+            camera1.SetActive(false);
+            moveCharacter.topView = true;
+            moveCharacter.sideView = false;
+        }
+        //Side View
+        else if(camera2.activeSelf == true)
+        {
+            camera1.SetActive(true);
+            camera2.SetActive(false);
+            moveCharacter.sideView = true;
+            moveCharacter.topView = false;
+        }
+        
+        
+    }
+    
+    public static void CanUseSpace()
+    {
+        space = !space;
+    }
+}
